3012fff664296c5c98d02a18d6569735ac38515e
Andrés Fernández <lafvg12@hotmail.com>
2022-01-17 21:51:40 -0500

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const routerApi = require('./routes/index');
 const { checkApiKey } = require('./middlewares/auth.handler');
 const {
@@ -10,7 +11,7 @@ const {
 } = require('./middlewares/error.handler');
 
 app.use(express.json());
-
+app.use(cors());
 routerApi(app);
 
 app.use(logErrors);
--- pass-verify.js
+++ pass-verify.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 async function hashPasword() {
-  const myPassword = 'holaf';
+  const myPassword = 'hola';
   const hashUser =
     '$2b$10$TPsDHjes2zz5YtCGeGdtTePNWUm4/E467ugV3TeV7z67ZwyQ4NLEe';
   const isMatch = await bcrypt.compare(myPassword, hashUser);
